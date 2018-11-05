7d18dbce262a55630049c66fc98ac77b0b692e07
filemon <filemon.michalski@gmail.com>
2018-11-05 13:40:52 +0100

    Serializers with ViewSets

--- backend/src/api/serializers.py
+++ backend/src/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Restaurant, Review
+from .models import Restaurant, Review, Opinion, Address
 
 
 class RestaurantSerializer(serializers.ModelSerializer):
@@ -8,8 +8,17 @@ class RestaurantSerializer(serializers.ModelSerializer):
         model = Restaurant
         fields = '__all__'
 
-
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
+        fields = '__all__'
+
+class OpinionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Opinion
+        fields = '__all__'
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
         fields = '__all__'
--- backend/src/api/views.py
+++ backend/src/api/views.py
@@ -1,14 +1,21 @@
 from rest_framework import viewsets
 
-from .models import Restaurant, Review
-from .serializers import RestaurantSerializer, ReviewSerializer
+from .models import Restaurant, Review, Address, Opinion
+from .serializers import RestaurantSerializer, ReviewSerializer, AddressSerializer, OpinionSerializer
 
 
 class RestaurantViewSet(viewsets.ModelViewSet):
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantSerializer
 
+class AddressViewSet(viewsets.ModelViewSet):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
 
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+
+class OpinionViewSet(viewsets.ModelViewSet):
+    queryset = Opinion.objects.all()
+    serializer_class = OpinionSerializer
