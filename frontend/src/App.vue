25cd4ab0a50c394728de841bc7b8f04e1883b53b
Klaudia Kozikowska <kozikowska95@gmail.com>
2018-11-10 00:36:33 +0100

    Home frontend (#3)
    * HomeView
    
    * Home, search, restaurantList

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,16 @@
 <template>
     <div id="app">
-        <div id="nav">
-            <router-link to="/">Home</router-link>
-            |
-            <router-link to="/newRestaurant">Nowa restauracja</router-link>
-            |
-            <router-link to="/newOpinion">Nowa opinia</router-link>
-        </div>
-        <router-view/>
+    <router-view/>
     </div>
-
 </template>
 
 <style lang="scss">
+    @import url('https://fonts.googleapis.com/css?family=Open+Sans');
+    body {
+        margin: 0;
+        height: 100%;
+        width: 100%;
+    }
     #app {
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
@@ -31,4 +29,26 @@
             }
         }
     }
+    .leftMenu {
+        background-color: white;
+        position: absolute;
+        border-right: solid 1px #979797;
+        height: 100%;
+        padding: 25px;
+        top: 0;
+        left: 0;
+    }
+    .leftMenu {
+        .material-icons {
+            display: block;
+            font-size: 30px;
+            color: var(--brown-grey);
+            margin-bottom: 20px;
+        }
+        :last-child {
+            bottom: 70px;
+            position: absolute;
+        }
+    }
+
 </style>
--- frontend/src/views/Home/Home.vue
+++ frontend/src/views/Home/Home.vue
@@ -1,26 +1,97 @@
 <template>
-  <div class="home">
-    <RestaurantList :restaurants="restaurants"/>
-  </div>
+    <div>
+        <div class="backgroundImage"
+             style="background-image: url(https://i2.wp.com/assets.yellowtrace.com.au/wp-content/uploads/2018/08/Noma-Restaurant-Copenhagen-by-Studio-Thulstrup-Yellowtrace-17.jpg)">
+        </div>
+        <div class="home">
+                <i class="material-icons" style="font-size:100px; margin: 80px;">
+                    fastfood
+                </i>
+            <el-row>
+                <Input id="searchInput"/>
+                <el-col :span="1">
+                    <el-button id="searchButton">
+                        Szukaj
+                    </el-button>
+                </el-col>
+            </el-row>
+            <el-row>
+                <h2>
+                    Ostatnio dodane:
+                </h2>
+            </el-row>
+            <RestaurantList/>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
-import {Component, Vue} from 'vue-property-decorator';
-import { Getter, Action } from 'vuex-class';
+    import {Component, Vue} from 'vue-property-decorator';
+    import {Getter, Action} from 'vuex-class';
 
-import RestaurantList from '@/components/restaurantList/RestaurantList.vue';
+    import RestaurantList from '@/components/restaurantList/RestaurantList.vue';
+    import Input from '@/components/input/Input';
 
-@Component({
-  components: {
-    RestaurantList,
-  },
-})
-export default class Home extends Vue {
-  @Action('getRestaurants') getRestaurants: any;
-  @Getter('getRestaurantList') restaurants: any;
+    @Component({
+        components: {
+            RestaurantList, Input
+        },
+    })
 
-  private created() {
-    this.getRestaurants();
-  }
-}
+    export default class Home extends Vue {
+        @Action('getRestaurants') getRestaurants: any;
+        @Getter('getRestaurantList') restaurants: any;
+
+        private created() {
+            this.getRestaurants();
+        }
+    }
 </script>
+
+<style lang="scss">
+    h2 {
+        font-family: 'Open Sans', sans-serif;
+        font-size: 28px;
+        font-weight: 500;
+        color: #191a1c;
+        margin: 65px;
+    }
+
+    #searchInput {
+        div {
+            input:focus {
+                border: solid 2px #f9d3a7;
+            }
+        }
+    }
+
+    #searchButton {
+        margin-left: 20px;
+        background-color: #f9d3a7;
+        &:focus, &:hover {
+            color: black;
+        }
+    }
+
+    .backgroundImage {
+        background-size: cover;
+        background-attachment: fixed;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        opacity: 0.1;
+        top: 0;
+        left: 0;
+    }
+
+    .backgroundImage:before {
+        content: '';
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 300px;
+        bottom: 0;
+        background: linear-gradient(0deg, rgba(255, 255, 255, 1) 20%, rgba(0, 212, 255, 0) 100%);
+        opacity: 1;
+    }
+</style>
