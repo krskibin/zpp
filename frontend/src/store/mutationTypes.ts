095fbbe0b8d5b57b548dd0d93afa24350e7acb6a
krskibin <chrisski96@gmail.com>
2018-11-10 13:08:05 +0100

    Configure router module to handle authorization

--- frontend/src/store/mutationTypes.ts
+++ frontend/src/store/mutationTypes.ts
@@ -3,5 +3,5 @@ export const AUTH_SAVE_USER = 'AUTH_SAVE_USER';
 export const AUTH_DELETE_USER = 'AUTH_DELETE_USER';
 export const AUTH_SAVE_INFO = 'AUTH_SAVE_INFO';
 
-// AUTH types
+// RESTAURANT types
 export const RESTAURANTS_LIST = 'RESTAURANTS_LIST';
