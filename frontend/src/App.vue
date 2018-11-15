9bc9047cf6da085dfd22e611e35e021d2e454c6b
Klaudia Kozikowska <kozikowska95@gmail.com>
2018-11-15 20:14:45 +0100

    Initial viewRestaurant

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,10 +12,9 @@
         width: 100%;
     }
     #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        font-family: 'Open Sans';
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
-        text-align: center;
         color: #2c3e50;
     }
 
---
+++ frontend/src/components/opinion/Opinion.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="containerOpinion">
+        <div class="titleOpinion">
+            <b>Tytuł opinii</b>
+        </div>
+        <Stars/>
+        treść opinii
+
+        <el-row class="textVersionOpinion">
+            <el-col :span="10">
+                Jedzenie
+            </el-col>
+            <el-col :span="10">
+                <Stars/>
+            </el-col>
+        </el-row>
+        <el-row class="textVersionOpinion">
+            <el-col :span="10">
+                Klimat
+            </el-col>
+            <el-col :span="10">
+                <Stars/>
+            </el-col>
+        </el-row>
+        <el-row class="textVersionOpinion">
+            <el-col :span="10">
+                Obsługa
+            </el-col>
+            <el-col :span="10">
+                <Stars/>
+            </el-col>
+        </el-row>
+        <el-row class="textVersionOpinion">
+            <el-col :span="10">
+                Cena
+            </el-col>
+            <el-col :span="10">
+                <Stars/>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+    import Stars from "../stars/Stars.vue"
+
+    export default {
+        components: {
+            Stars
+        },
+    };
+</script>
+
+<style lang="scss">
+    .containerOpinion {
+        border-radius: 5px;
+        border: solid 2px #d8d8d8;
+        text-align: left;
+        padding: 10px;
+    }
+
+    .textVersionOpinion {
+        font-weight: bold;
+        padding: 10px;
+    }
+</style>
