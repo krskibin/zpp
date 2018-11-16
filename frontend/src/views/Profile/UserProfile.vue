74cc845da7c4412b531381a07a1dcd1f9005430f
dparzyk <dawidparzyk@gmail.com>
2018-11-16 23:03:45 +0100

    Update UserProfile.vue

--- frontend/src/views/Profile/UserProfile.vue
+++ frontend/src/views/Profile/UserProfile.vue
@@ -1,13 +1,25 @@
 <template>
-  <h1>Profile</h1>
+<div>
+  <h1>Login</h1>
+  <Form>
+	<FormField label="Email address" htmlFor="basic-form-input-email">
+		<FormInput autoFocus type="email" placeholder="Enter email" name="basic-form-input-email" />
+	</FormField>
+	<FormField label="Password" htmlFor="basic-form-input-password">
+		<FormInput type="password" placeholder="Password" name="basic-form-input-password" />
+	</FormField>
+	<FormField>
+		<Checkbox label="Check it" />
+	</FormField>
+	<Button submit>Submit</Button>
+</Form>
+</div>
 </template>
 
-<script lang="ts">
-import {Vue} from 'vue-property-decorator';
-
-export default class UserProfile extends Vue {
-  name = 'user-profile';
-}
+<script>
+export default {
+  name: 'user-login',
+};
 </script>
 
 <style lang="scss">
