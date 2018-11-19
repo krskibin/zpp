3266188f68d7b7d08aa66301beb1826d84c8f16f
Klaudia Kozikowska <kozikowska95@gmail.com>
2018-11-19 22:42:06 +0100

    RestaurantView v2 (#11)
    * RestaurantView v2
    
    * Change space
    
    * Fix to fix spaces

--- frontend/src/views/RestaurantView/RestaurantView.vue
+++ frontend/src/views/RestaurantView/RestaurantView.vue
@@ -1,90 +1,65 @@
 <template>
   <div>
     <el-row>
-      <el-col :span="24"><h1 class="centerText">Nazwa restauracji</h1></el-col>
+      <el-col :span="24"><h1 class="centerText">{{restaurantInfo.restaurant.name}}</h1></el-col>
     </el-row>
     <el-row>
-      <el-col :span="24"><h3 class="centerText">rodzaj kuchni</h3></el-col>
+      <el-col :span="24"><h3 class="centerText">{{restaurantInfo.restaurant.foodType}}</h3></el-col>
     </el-row>
     <el-row>
       <el-col :span="24">
-        <Stars class="centerText"/>
+        <el-rate v-model="restaurantInfo.restaurant.longitude" disabled class="starsBig"/>
       </el-col>
     </el-row>
     <el-row>
       <el-col :span="24" class="centerText">
-        <el-button>
+        <el-button class="buttonStyle">
           Dodaj opinię
         </el-button>
       </el-col>
     </el-row>
     <el-row>
       <el-col :span="10" :offset="3">
-        <h2>Opis restauracji</h2>
+        <h2 class="left">Opis restauracji</h2>
       </el-col>
     </el-row>
     <el-row>
       <el-col :span="18" :offset="3" class="addInfo">
-        <p>{{restaurant.shortReview}}</p>
+        <p>{{restaurantInfo.restaurant.shortReview}}</p>
       </el-col>
     </el-row>
     <el-row>
       <el-col :span="10" :offset="3">
-        <h2>Dodatkowe informacje</h2>
+        <h2 class="left">Dodatkowe informacje</h2>
       </el-col>
     </el-row>
     <el-row>
       <el-col :span="18" :offset="3" class="addInfo">
-        <p>Dania wegetariańskie: {{restaurant.vegetarianOption ? "Tak" : "Nie"}}</p>
-        <p>Dania wegańskie: {{restaurant.veganOption ? "Tak" : "Nie"}}</p>
+        <p>Dania wegetariańskie: {{restaurantInfo.restaurant.vegetarianOption ? "Tak" : "Nie"}}</p>
+        <p>Dania wegańskie: {{restaurantInfo.restaurant.veganOption ? "Tak" : "Nie"}}</p>
         <p>Dostępna toaleta:</p>
         <p>Dozwolone psy:</p>
       </el-col>
     </el-row>
-    <el-row>
-      <el-col :span="24"><h2 class="centerText">Opinie o lokalu</h2></el-col>
-    </el-row>
-    <el-row class="rowOpinions">
-      <el-col :span="6" :offset="3">
-      </el-col>
-    </el-row>
-
-    <el-row>
-      <el-col :span="10" :offset="3">
-        <h2>Dodatkowe informacje</h2>
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="18" :offset="3">
-        <p>Dania wegetariańskie:</p>
-        <p>Dania wegańskie:</p>
-        <p>Dostępna toaleta:</p>
-        <p>Dozwolone psy:</p>
-      </el-col>
-    </el-row>
-
     <el-row>
       <el-col :span="24"><h1 class="centerText">Opinie o lokalu</h1></el-col>
     </el-row>
 
     <el-row class="rowOpinions">
       <el-col :span="6" :offset="3">
-        <Opinion/>
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script lang="ts">
+
 import {Component, Vue} from 'vue-property-decorator';
 import {Getter, Action} from 'vuex-class';
-
-import Stars from '@/components/stars/Stars.vue';
 import Opinion from '../../components/opinion/Opinion.vue';
 
 @Component({
   components: {
-    Stars,
     Opinion,
   },
 })
@@ -92,13 +67,18 @@ export default class RestaurantView extends Vue {
   @Action('getRestaurant') getRestaurant: any;
   @Getter('getRestaurantElement') restaurant: any;
 
+  get restaurantInfo(): any {
+    return this.$store.state.restaurant;
+  }
+
   private created() {
     this.getRestaurant(this.$route.params.id);
   }
+
 }
 </script>
 
-<style>
+<style lang="scss">
 .rowOpinions {
   margin: 10px 0 10px 0;
   :nth-child(2) {
@@ -145,12 +125,14 @@ h3.centerText {
     margin: 0;
     font-size: 18px;
     font-weight: bold;
+    text-align: left;
   }
 }
 
-h2 {
+h2.left {
   margin: 30px 0 30px 0;
   font-weight: bold;
   color: #191a1c;
+  text-align: left;
 }
 </style>
