0f9edf0a4def756603e08d48df8bd1d1f1b31510
Filemon279 <280202@uwr.edu.pl>
2018-11-21 00:20:56 +0100

    Adjust backend models (#14)
    * without Inlines
    
    * saved
    
    * saved
    
    * Adjust backend models
    
    * saved
    
    * Adjusted models, inlines
    
    * One calculation

--- backend/src/api/models.py
+++ backend/src/api/models.py
@@ -9,48 +9,44 @@ RATING_SCALE = (
     (5, 'Best Place EVER')
 )
 
+FEATURES_CHOICES = (
+    ('WiFi', 'WiFi'),
+    ('Toliet', 'Toliet'),
+    ('Dogs allowed', 'Dogs allowed'),
+    ('Climatization', 'Climatization'),
+    ('Outdoor tabkle', 'Outdoor tabkle')
+)
 
-class Feature(models.Model):
-    name = ""
-
-
-class Wifi(Feature):
-    name = "WiFi"
-
-
-class DogsAllowed(Feature):
-    name = "Dogs Allowed"
-
-
-class Toilet(Feature):
-    name = "Toilet"
-
-
-class ChangingRoom(Feature):
-    name = "Changing Room"
-
-
-class Address(models.Model):
-    country = models.CharField(max_length=40, default='Poland')
-    city = models.CharField(max_length=40, default='Wroclaw')
-    street = models.CharField(max_length=70)
-    local_number = models.CharField(max_length=5)
-    postcode = models.CharField(max_length=6)
 
 
 class Restaurant(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, default='Restaurant name')
-    longitude = models.DecimalField(max_digits=9, decimal_places=6)   # Position of restaurant on the map
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)  # Position of restaurant on the map
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name='address')
     food_type = models.CharField(max_length=40, default='Kuchnia polska')
-    avg_rating = models.DecimalField(max_digits=3, decimal_places=2)
+    #avg_rating = models.DecimalField(max_digits=3, decimal_places=2)
     price_rating = models.DecimalField(max_digits=3, decimal_places=2)
     vegan_option = models.BooleanField(default=False)
     vegetarian_option = models.BooleanField(default=False)
-    # features = models.ForeignKey(Feature, on_delete=models.CASCADE, related_name='features')
     short_review = models.CharField(max_length=300, default='Short Review')
+
+    @property
+    def avg_rating(self):
+        opinions = Opinion.objects.filter(restaurant=self)
+        avgRating = 0
+        for opinion in opinions:
+            avgRating += ((opinion.food_review.rating+opinion.climate_review.rating+opinion.staff_review.rating+opinion.price_review.rating)/4)
+        return avgRating/opinions.size
+    
+
+class Address(models.Model):
+    restaurant = models.OneToOneField(Restaurant, on_delete=models.CASCADE, related_name='address')
+    country = models.CharField(max_length=40, default='Poland')
+    city = models.CharField(max_length=40, default='Wroclaw')
+    street = models.CharField(max_length=70)
+    local_number = models.CharField(max_length=5)
+    postcode = models.CharField(max_length=6)
 
 
 class Review(models.Model):
@@ -69,3 +65,8 @@ class Opinion(models.Model):
     staff_review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='staffReview')
     price_review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='priceReview')
     receipt_number = models.CharField(max_length=30, default='#')
+
+class Features(models.Model):
+    features = models.CharField(max_length=40, choices=FEATURES_CHOICES)
+    option = models.ForeignKey(Opinion, on_delete=models.CASCADE)
+
