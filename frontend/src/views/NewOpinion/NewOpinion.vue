905341b25405ca66473e3e9a218c8955ffa38e63
krskibin <chrisski96@gmail.com>
2018-12-18 03:09:34 +0100

    Margin -250px position: fixed na caly komponent

--- frontend/src/views/NewOpinion/NewOpinion.vue
+++ frontend/src/views/NewOpinion/NewOpinion.vue
@@ -1,35 +1,36 @@
 <template>
   <div class="newRestaurant">
-    <el-form model="ruleForm" ref="ruleForm">
+    <el-form :model="ruleForm" ref="ruleForm" :rules="rules2">
       <el-row>
         <h1>Dodaj nową opinię</h1>
       </el-row>
       <el-row :gutter="20">
         <h2 class="h2o">Wybierz restaurację</h2>
       </el-row>
-      <el-row :gutter="500">
+      <el-row :gutter="10">
         <el-form-item prop="name">
           <el-autocomplete
-              class="inline-input"
-              v-model="ruleForm.name"
-              placeholder="Wpisz nazwę restauracji"
-              :trigger-on-focus="false"
-              v-bind:class="{ 'is-invalid': missingName}"
-          ></el-autocomplete>
+            class="inline-input"
+            v-model="ruleForm.name"
+            placeholder="Wpisz nazwę restauracji"
+            :trigger-on-focus="false"
+            v-bind:class="{ 'is-invalid': missingName}"
+            ></el-autocomplete>
           <div class="invalid-feedback marginLeft" v-show="missingName">Wybierz restaurację.</div>
         </el-form-item>
       </el-row>
       <el-row :gutter="20">
         <h2 class="h2o" style="margin-top: 30px;">Ocena ogólna</h2>
       </el-row>
-      <el-row :gutter="500">
-        <el-rate class="general-rate"
-                 v-model="value5"
-                 show-score
-                 text-color="#f9d3a7"
-                 score-template="{value} points"
-        >
-        </el-rate>
+      <el-row :gutter="20">
+        <el-form-item prop="value1" class="general-rate">
+          <el-rate v-model="ruleForm.value1"
+                   show-score
+                   text-color="#f9d3a7"
+                   score-template="{value} points"
+                   >
+          </el-rate>
+        </el-form-item>
       </el-row>
 
       <el-row :gutter="20">
@@ -38,14 +39,14 @@
       <el-row style="margin-top: 4px;">
         <el-form-item prop="title">
           <el-input class="input" placeholder="Wpisz tytuł recenzji" v-model="ruleForm.title"
-                    v-bind:class="{ 'is-invalid': missingTitle}"></el-input>
+                                                                     v-bind:class="{ 'is-invalid': missingTitle}"></el-input>
           <div class="invalid-feedback" v-show="missingTitle">Wpisz tytuł recenzji.</div>
         </el-form-item>
       </el-row>
       <el-form-item prop="description">
         <el-row style="margin-top: 20px;">
           <el-input class="input" type="textarea" :rows="6" placeholder="Napisz swoją recenzję"
-                    v-model="ruleForm.description" v-bind:class="{ 'is-invalid': missingDescription}"></el-input>
+                                                            v-model="ruleForm.description" v-bind:class="{ 'is-invalid': missingDescription}"></el-input>
           <div class="invalid-feedback" v-show="missingDescription">Musisz uzupełnić to pole.</div>
         </el-row>
       </el-form-item>
@@ -55,25 +56,29 @@
             <h3>Jedzenie</h3>
           </el-row>
           <el-row :gutter="500">
-            <el-rate
-                v-model="value5"
+            <el-form-item prop="value2">
+              <el-rate
+                v-model="ruleForm.value2"
                 show-score
-                text-color="#f9d3a7"
+                text-color1="#f9d3a7"
                 score-template="{value} points">
-            </el-rate>
+              </el-rate>
+            </el-form-item>
           </el-row>
         </el-col>
         <el-col :span="12">
-          <el-row :gutter="20">
+          <el-row :gutter="4">
             <h3>Klimat</h3>
           </el-row>
-          <el-row :gutter="500">
-            <el-rate
-                v-model="value5"
+          <el-row :gutter="20">
+            <el-form-item prop="value3">
+              <el-rate
+                v-model="ruleForm.value3"
                 show-score
                 text-color="#f9d3a7"
                 score-template="{value} points">
-            </el-rate>
+              </el-rate>
+            </el-form-item>
           </el-row>
         </el-col>
       </el-row>
@@ -83,135 +88,187 @@
             <h3>Obsługa</h3>
           </el-row>
           <el-row :gutter="500">
-            <el-rate
-                v-model="value5"
+            <el-form-item prop="value4">
+              <el-rate
+                v-model="ruleForm.value4"
                 show-score
                 text-color="#f9d3a7"
                 score-template="{value} points">
-            </el-rate>
+              </el-rate>
+            </el-form-item>
           </el-row>
         </el-col>
         <el-col :span="12">
-          <el-row :gutter="20">
+          <el-row :gutter="4">
             <h3>Cena</h3>
           </el-row>
-          <el-row :gutter="500">
-            <el-rate
-                v-model="value5"
+          <el-row :gutter="20">
+            <el-form-item prop="value5">
+              <el-rate
+                v-model="ruleForm.value5"
                 show-score
                 text-color="#f9d3a7"
                 score-template="{value} points">
-            </el-rate>
+              </el-rate>
+            </el-form-item>
           </el-row>
         </el-col>
       </el-row>
       <el-row style="margin-top: 40px; margin-bottom: 50px;">
         <el-button @click="submitForm">Dodaj opinię</el-button>
-
       </el-row>
     </el-form>
   </div>
 </template>
 
-<script lang="ts">
-  interface ruleForm {
-    name: string;
-    title: string;
-    description: string;
-  }
+<script>
+import {Component, Vue} from 'vue-property-decorator';
+import Input from '../../components/input/Input.vue';
+import Stars from '../../components/stars/Stars.vue';
 
-  import {Component, Vue} from 'vue-property-decorator';
-  import Input from '../../components/input/Input.vue';
-  import Stars from '../../components/stars/Stars.vue';
-
-  @Component({
-    components: {
-      Input,
-      Stars,
-    },
-  })
-  export default class NewOpinion extends Vue {
-    ruleForm: ruleForm = {
-      name: '',
-      title: '',
-      description: '',
+export default {
+  components: {
+    Input,
+    Stars,
+  },
+  data() {
+    const checkRate = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('Please input the rate'));
+      }
+      setTimeout(() => {
+        if (!Number.isInteger(value)) {
+          callback(new Error('Please input digits'));
+        } else {
+          if (value < 1) {
+            callback(new Error('Age must be greater than 0'));
+          } else {
+            callback();
+          }
+        }
+      }, 1000);
     };
-    missingName: boolean = false;
-    missingTitle: boolean = false;
-    missingDescription: boolean = false;
-
+    return {
+      rules2: {
+        value1: [
+          { validator: checkRate, trigger: 'blur' },
+        ],
+        value2: [
+          { validator: checkRate, trigger: 'blur' },
+        ],
+        value3: [
+          { validator: checkRate, trigger: 'blur' },
+        ],
+        value4: [
+          { validator: checkRate, trigger: 'blur' },
+        ],
+        value5: [
+          { validator: checkRate, trigger: 'blur' },
+        ],
+      },
+      ruleForm: {
+        name: '',
+        title: '',
+        description: '',
+        value1: 0,
+        value2: 0,
+        value3: 0,
+        value4: 0,
+        value5: 0,
+      },
+      missingName: false,
+      missingTitle: false,
+      missingDescription: false,
+    };
+  },
+  methods: {
     submitForm() {
       this.ruleForm.name === '' ? this.missingName = true : this.missingName = false;
       this.ruleForm.title === '' ? this.missingTitle = true : this.missingTitle = false;
       this.ruleForm.description === '' ? this.missingDescription = true : this.missingDescription = false;
-    }
-  }
+
+      this.$refs.ruleForm.validate((valid) => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          alert('Error!');
+          return false;
+        }
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss">
 
-  h1 {
-    font-size: 36px;
-    font-weight: 800;
-    color: #191a1c;
-    margin: 60px;
-  }
+h1 {
+  font-size: 36px;
+  font-weight: 800;
+  color: #191a1c;
+  margin: 60px;
+}
 
-  .h2o {
-    font-size: 18px;
-    font-weight: 800;
-    color: #191a1c;
-    float: left;
-    margin: 16px;
-    margin-left: 100px;
-  }
+.h2o {
+  font-size: 18px;
+  font-weight: 800;
+  color: #191a1c;
+  float: left;
+  margin: 16px;
+  margin-left: 100px;
+}
 
-  el-button {
-    margin-top: 30px;
-  }
+el-button {
+  margin-top: 30px;
+}
 
-  .inline-input {
-    width: 600px;
-    float: left;
-    margin-left: 340px;
-  }
+.inline-input {
+  width: 600px;
+  float: left;
+  margin-left: 95px;
+}
 
-  .general-rate {
-    float: left;
-    margin-left: 340px;
-  }
+.general-rate {
+  float: left;
+  margin-left: 97px;
 
-  .input {
-    width: 600px;
-    float: left;
-    margin-left: 90px;
-  }
+}
 
-  .newRestaurant {
-    position: fixed;
-    width: 100%;
-    height: 100%;
-  }
+.input {
+  width: 600px;
+  float: left;
+  margin-left: 90px;
+}
 
-  .is-invalid {
-    textarea {
-      border-color: rosybrown;
-    }
-    input {
-      border-color: rosybrown;
-    }
-  }
+.newRestaurant {
+  width: 100%;
+  height: 100%;
+}
 
-  .invalid-feedback {
-    position: absolute;
-    border: 0;
-    bottom: -30px;
-    margin-left: 100px;
-    font-size: 12px;
-    color: rosybrown;
+.is-invalid {
+  textarea {
+    border-color: rosybrown;
   }
+  input {
+    border-color: rosybrown;
+  }
+}
 
-  .marginLeft {
-    margin-left: 353px;
-  }
+.invalid-feedback {
+  position: absolute;
+  border: 0;
+  bottom: -30px;
+  margin-left: 100px;
+  font-size: 12px;
+  color: rosybrown;
+}
+
+.el-form-item__error {
+  position: relative;
+  margin-left: -50px;
+}
+
+.marginLeft {
+  margin-left: 353px;
+}
 </style>
