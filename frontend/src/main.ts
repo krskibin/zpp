eb7ba061a311610afb1776d12b6711945c471d6b
krskibin <chrisski96@gmail.com>
2019-01-15 01:09:21 +0100

    Change font

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,14 +3,10 @@ import App from './App.vue';
 import router from './router/';
 import store from './store/store';
 import Element from 'element-ui';
-
-import 'element-ui/lib/theme-chalk/index.css';
 import locale from 'element-ui/lib/locale/lang/en';
-
-Vue.use(Element, {locale});
-
+import EvaIcons from 'vue-eva-icons'
 import 'element-ui/lib/theme-chalk/index.css';
-
+Vue.use(EvaIcons);
 
 Vue.use(Element);
 Vue.config.productionTip = false;
