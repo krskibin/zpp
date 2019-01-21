d51dcb3304218e3624a9caf981a0990cf34dad79
filemon <filemon279@o2.pl>
2019-01-21 16:00:21 +0100

    Restored

--- frontend/src/store/modules/restaurantId.ts
+++ frontend/src/store/modules/restaurantId.ts
@@ -1,17 +1,25 @@
 import {RESTAURANT_ID} from '@/store/mutationTypes';
+import {OPINION_ID} from '@/store/mutationTypes';
+
 import HTTP from '@/utils/http';
 
 
 interface State {
   restaurant: object[];
+  opinions: object[];
+
 }
 
 const state: State = {
   restaurant: [],
+  opinions: [],
+
 };
 
 const getters = {
   getRestaurantElement: (storeState: State) => storeState.restaurant,
+  getOpinions: (storeState: State) => storeState.opinions,
+
 };
 
 const actions = {
@@ -29,11 +37,31 @@ const actions = {
       };
     }
   },
+  async getRestaurantOpinion(ctx: any, id: number) {
+    try {
+      const response = await HTTP.get(`v1/opinions/`);
+      if (response.data  && response.data.results instanceof Array) {
+        function isRestaurantOpinion(elem, i, array){
+            return elem.restaurant==id
+        }
+        const opinions = response.data.results.filter(isRestaurantOpinion);
+        ctx.commit(OPINION_ID, opinions);
+      }
+    } catch (error) {
+      return {
+        success: 'false',
+        message: error,
+      };
+    }
+  },
 };
 
 const mutations = {
   [RESTAURANT_ID](storeState: State, payload: object[]) {
     storeState.restaurant = payload;
+  },
+  [OPINION_ID](storeState: State, payload: object[]) {
+    storeState.opinions = payload;
   },
 };
 
