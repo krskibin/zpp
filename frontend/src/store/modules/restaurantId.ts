35a33bb733c51f84547f43d4ea84fa98ceeae726
filemon <filemon279@o2.pl>
2019-01-21 15:49:03 +0100

    FrontEnd Fixes

---
+++ frontend/src/store/modules/restaurantId.ts
@@ -0,0 +1,45 @@
+import {RESTAURANT_ID} from '@/store/mutationTypes';
+import HTTP from '@/utils/http';
+
+
+interface State {
+  restaurant: object[];
+}
+
+const state: State = {
+  restaurant: [],
+};
+
+const getters = {
+  getRestaurantElement: (storeState: State) => storeState.restaurant,
+};
+
+const actions = {
+  async getRestaurant(ctx: any, id: number) {
+    try {
+      const response = await HTTP.get(`v1/restaurants/${id}`);
+      if (response.data) {
+        const restaurant = response.data;
+        ctx.commit(RESTAURANT_ID, restaurant);
+      }
+    } catch (error) {
+      return {
+        success: 'false',
+        message: error,
+      };
+    }
+  },
+};
+
+const mutations = {
+  [RESTAURANT_ID](storeState: State, payload: object[]) {
+    storeState.restaurant = payload;
+  },
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+};
