cd559d2d7eb1308c08b94aaa98d7924c5c2ed8dc
filemon <filemon279@o2.pl>
2019-01-21 15:46:52 +0100

    FrontEnd Fixes

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,6 +6,7 @@ import Element from 'element-ui';
 import locale from 'element-ui/lib/locale/lang/en';
 import EvaIcons from 'vue-eva-icons'
 import 'element-ui/lib/theme-chalk/index.css';
+import './assets/styles/globalStyle.css'
 Vue.use(EvaIcons);
 
 Vue.use(Element, {locale});
